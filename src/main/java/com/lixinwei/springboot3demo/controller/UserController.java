f116a8145b892e7377e2f961a9ff2a2c4634f65c
lixinwei <2382454187@qq.com>
2023-09-06 12:27:10 +0800

    init commit

---
+++ src/main/java/com/lixinwei/springboot3demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.lixinwei.springboot3demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lixinwei
+ * @date 2023/9/6 10:59
+ */
+@RestController("/user")
+public class UserController {
+
+
+}
---
+++ src/main/java/com/lixinwei/springboot3demo/pojo/User.java
@@ -0,0 +1,15 @@
+package com.lixinwei.springboot3demo.pojo;
+
+/**
+ * @author lixinwei
+ * @date 2023/9/6 11:03
+ */
+@Data
+public class User {
+    private String id;
+
+    private String name;
+
+    private String address;
+
+}
